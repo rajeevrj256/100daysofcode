2143777abad74517dc5ea7f919fff71622f423d3
rajeevrj256 <rjrajeev5918@gmail.com>
2023-10-03 22:37:45 +0530

    day2

---
+++ day2/ans.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans=0
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    ans=ans+1
+        
+
+        return ans
