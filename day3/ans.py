f0ce0461a02a548e65fa37fa17ca7666ec60d665
rajeevrj256 <rjrajeev5918@gmail.com>
2023-10-04 22:04:43 +0530

    day3

---
+++ day3/ans.py
@@ -0,0 +1,35 @@
+class MyHashMap(object):
+    
+    def __init__(self):
+        self.MyhashMap=[-1]*1000001
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        self.MyhashMap[key]= value
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        return self.MyhashMap[key]
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.MyhashMap[key]= -1
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
