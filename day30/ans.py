d75f9e1e4359289d47d5180ac1fbb3da9c30353f
rajeevrj256 <rjrajeev5918@gmail.com>
2023-10-30 20:50:48 +0530

    day30

---
+++ day30/ans.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortByBits(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        arr.sort(key= lambda num: (bin(num).count('1'),num))
+        return arr
+
+        
